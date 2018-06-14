ab0078733eed2565ca1e2cd80f082d37857f03c9
Oleh Bai <oleh.bai@eliftech.com>
2018-06-14 20:01:26 +0300

    added multi language background

---
+++ src/Languages.js
@@ -0,0 +1,36 @@
+
+const appWordStructure = {
+  appName: 'app',
+  name: 'Name',
+  secondName: 'Second name',
+  age: 'Age'
+}
+
+const appWordStructureUA = {
+  appName: 'Назва програми',
+  name: 'Імʼя',
+  secondName: 'Прізвище',
+  age: 'Вік',
+
+}
+
+const LANGUAGES_ISO = {
+  en: appWordStructure,
+  ua: appWordStructureUA,
+}
+
+class WordManager {
+  constructor() {
+    this.currentLanguage = 'en';
+    this.word = LANGUAGES_ISO[this.currentLanguage]
+  }
+
+  setCurrentLanguage (ln) {
+    console.log('New language '+ln.slice(0,2).toLowerCase())
+    this.currentLanguage = this.currentLanguage === 'ua' ? 'en' : 'ua';
+    this.word = LANGUAGES_ISO[this.currentLanguage];
+  }
+
+}
+
+export default new WordManager();
